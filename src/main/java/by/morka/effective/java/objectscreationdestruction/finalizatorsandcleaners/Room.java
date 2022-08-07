727893136fc24c7e2d491ad56076f380d32f62c6
Mark Putyato <mark_putyato@mail.ru>
2022-08-07 14:51:33 +0300

    2: Avoid finalizers and cleaners

---
+++ src/main/java/by/morka/effective/java/objectscreationdestruction/finalizatorsandcleaners/Room.java
@@ -0,0 +1,38 @@
+package by.morka.effective.java.objectscreationdestruction.finalizatorsandcleaners;
+
+import java.lang.ref.Cleaner;
+
+public class Room implements AutoCloseable {
+    private static final Cleaner cleaner = Cleaner.create();
+
+    // Ресурс, требующий очистки. He должен ссылаться на Room?
+    private static class State implements Runnable {
+        int numJunkPiles; // Количество мусора в комнате
+
+        State(int numJunkPiles) {
+            this.numJunkPiles = numJunkPiles;
+        }
+
+        // Вызывается методом close или cleaner
+        @Override
+        public void run() {
+            System.out.println("Cleaning room");
+            numJunkPiles = 0;
+        }
+    }
+
+    // Состояние комнаты, используется совместно с cleanable
+    private final State state;
+    // Очистка комнаты, когда она готова для сборщика мусора
+    private final Cleaner.Cleanable cleanable;
+
+    public Room(int numJunkPiles) {
+        state = new State(numJunkPiles);
+        cleanable = cleaner.register(this, state);
+    }
+
+    @Override
+    public void close() {
+        cleanable.clean();
+    }
+}
