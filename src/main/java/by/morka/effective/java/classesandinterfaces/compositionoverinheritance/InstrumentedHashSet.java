a89d543b3e5c054c8442439f6032949749e02577
Mark Putyato <mark_putyato@mail.ru>
2022-08-08 23:05:08 +0300

    4: Composition over inheritance

---
+++ src/main/java/by/morka/effective/java/classesandinterfaces/compositionoverinheritance/InstrumentedHashSet.java
@@ -0,0 +1,46 @@
+package by.morka.effective.java.classesandinterfaces.compositionoverinheritance;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+// Broken - Inappropriate use of inheritance!
+public class InstrumentedHashSet<E> extends HashSet<E> {
+    // The number of attempted element insertions
+    private int addCount = 0;
+
+    public InstrumentedHashSet() {
+    }
+
+    public InstrumentedHashSet(int initCap, float loadFactor) {
+        super(initCap, loadFactor);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    /*
+    We don't know that addAll() calls all() under the hood,
+    the contract might change over time and our class will be broken.
+    So such inheritance is broken.
+    Even if API is documented well, still no trust.
+     */
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+
+    public static void main(String[] args) {
+        InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
+        s.addAll(List.of("Snap", "Crackle", "Pop"));
+        System.out.println(s.getAddCount());
+    }
+}
---
+++ src/main/java/by/morka/effective/java/classesandinterfaces/compositionoverinheritance/InstrumentedSet.java
@@ -0,0 +1,36 @@
+package by.morka.effective.java.classesandinterfaces.compositionoverinheritance;
+
+import java.util.*;
+
+/*
+Decorator pattern in action!
+ */
+// Wrapper class - uses composition in place of inheritance
+/*
+Now api is not broken
+ */
+public class InstrumentedSet<E> extends ForwardingSet<E> {
+    private int addCount = 0;
+
+    public InstrumentedSet(Set<E> s) {
+        super(s);
+    }
+
+    @Override public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+    @Override public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+    public int getAddCount() {
+        return addCount;
+    }
+
+    public static void main(String[] args) {
+        InstrumentedSet<String> s = new InstrumentedSet<>(new HashSet<>());
+        s.addAll(List.of("Snap", "Crackle", "Pop"));
+        System.out.println(s.getAddCount());
+    }
+}
