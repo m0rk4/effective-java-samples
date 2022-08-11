7e62c140418ffce981479257f9e2c3339049e46b
Mark Putyato <mark_putyato@mail.ru>
2022-08-11 22:03:24 +0300

    5: Prefer generic methods

---
+++ src/main/java/by/morka/effective/java/genericprogramming/genericmethods/GenericSingletonFactory.java
@@ -0,0 +1,27 @@
+package by.morka.effective.java.genericprogramming.genericmethods;
+
+import java.util.function.UnaryOperator;
+
+// Generic singleton factory pattern
+public class GenericSingletonFactory {
+    // Generic singleton factory pattern
+    private static final UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+
+    @SuppressWarnings("unchecked")
+    public static <T> UnaryOperator<T> identityFunction() {
+        return (UnaryOperator<T>) IDENTITY_FN;
+    }
+
+    // Sample program to exercise generic singleton
+    public static void main(String[] args) {
+        String[] strings = {"jute", "hemp", "nylon"};
+        UnaryOperator<String> sameString = identityFunction();
+        for (String s : strings)
+            System.out.println(sameString.apply(s));
+
+        Number[] numbers = {1, 2.0, 3L};
+        UnaryOperator<Number> sameNumber = identityFunction();
+        for (Number n : numbers)
+            System.out.println(sameNumber.apply(n));
+    }
+}
